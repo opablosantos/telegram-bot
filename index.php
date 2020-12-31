43b5d879f8113fdcd02ec07a17c2db6c18c6076f
Pablo Santos <opablosantos@gmail.com>
2020-12-31 17:43:06 -0300

    Criei o index
    O value deve contar o chat id dos contatos

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+<h1>P3 bot</h1>
+<form method="get" action="p3bot.php">
+    Digite sua mensagem: <input type="text" name="txt"/><br/>
+    Selecione o contato: <select name="cont" id="cont">
+        <option value="">Contato 1</option>
+        <option value="">Contato 2</option>
+        <option value="">Contato 3</option>
+        <option value="">Contato 4</option>
+    </select><br/>
+    <input type="submit" value="enviar"/>
+</form>
+</body>
+</html>
