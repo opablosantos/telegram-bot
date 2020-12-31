d1dd7e6c51a0f8b019b66a0aea9d575f22fde47d
Pablo Santos <opablosantos@gmail.com>
2020-12-31 17:44:17 -0300

    Criei o motor de envio da mensagem
    O token deve ser preenchido com o token do bot da api do telegram bot

---
+++ p3bot.php
@@ -0,0 +1,19 @@
+<?php
+
+$token = "";
+
+$user_id = $_GET['cont'];
+
+$msg = $_GET['txt'];
+
+$request_params = [
+    'chat_id'=> $user_id,
+    'text'=> $msg
+];
+
+$request_url = 'https://api.telegram.org/bot'. $token. '/sendMessage?'. http_build_query($request_params);
+
+file_get_contents($request_url);
+?>
+<h1>Sua mensagem foi enviada com sucesso!</h1><br/>
+<a href="index.php">Voltar</a>
